a60e144f34d6ed3114e87182ca04cf78a6e321c0
Jose David <jose7312000@gmail.com>
2022-04-18 22:15:48 -0500

    Importando HasApiTokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+//Le estamos diciendo al modelo que estamos trabajando con un sistema basado en tokens
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
